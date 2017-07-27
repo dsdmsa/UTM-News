03cc71b99f437df623e1c057fce0aea17336531c
Mihail <urmanschi.mihail@gmail.com>
2017-07-27 12:01:40 +0300

    minor fixes

--- app/src/main/java/dsdmsa/utmnews/data/interactor/CategoryNewsInteractor.java
+++ app/src/main/java/dsdmsa/utmnews/data/interactor/CategoryNewsInteractor.java
@@ -25,32 +25,4 @@ public class CategoryNewsInteractor {
     public Observable<List<SimplePost>> getCategories(int categoryId, int page) {
         return getNewsUseCase.getNews(api.getPostsByCategories(categoryId, Constants.ITEMS_PER_PAGE, page));
     }
-//                .flatMapIterable(list -> list)
-//                .map(SimplePostAdapter::getSimplePost)
-//                .map(post -> {
-//                    post.setCategory(appDb.getCategoryDao().getCategory(post.getCategoryId()).getName());
-//                    return post;
-//                })
-//                .map(post -> {
-//                    post.setBookmarked(appDb.getPostDao().getPostById(post.getId()) != null);
-//                    Timber.d("post is bookmrked " + post.isBookmarked());
-//                    return post;
-//                })
-//                .map(post -> {
-//                    String date = post.getDate().substring(0, post.getDate().indexOf('T'));
-//                    date = date.replace('-', ' ');
-//                    String[] dates = date.split(Pattern.quote(" "));
-//                    date = String.format("%s %s %s", dates[2], getMonth(Integer.valueOf(dates[1])), dates[0]);
-//                    post.setDate(date);
-//                    return post;
-//                })
-//                .toList()
-//                .toObservable()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread());
-//    }
-//
-//    private String getMonth(int month) {
-//        return new DateFormatSymbols().getMonths()[month - 1];
-//    }
 }
